14c054fde41d93e9da63ba5fd7d11f4b3262c948
amul kumar <amul.kumar.min21@itbhu.ac.in>
2025-08-02 04:46:19 +0530

    fix marker not loaded in production

--- src/components/Mapview.jsx
+++ src/components/Mapview.jsx
@@ -1,6 +1,20 @@
 import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
 import mines from "../data";
 import MarkerPopup from "./MarkerPopup";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet's default icon issue with Webpack/Vite
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 const Mapview = () => {
     return (
         <MapContainer
