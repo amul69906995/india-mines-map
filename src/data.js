f1c3919879996418de7ee3956d83a202e9569387
amul kumar <amul.kumar.min21@itbhu.ac.in>
2026-04-10 08:56:46 +0530

    pdf added and data modified

--- src/data.js
+++ src/data.js
@@ -1,217 +1,357 @@
 const mines =[
+ 
   {
     "name": "Kollur Diamond Mine",
     "state": "Andhra Pradesh",
+    "country": "India",
     "minetype": "diamond",
-    "lat": 16.7,
+    "lat": 16.71667,
     "lng": 80.05,
-    "hasPdf":true
+    "hasPdf": true
   },
-   {
+  {
     "name": "Jharia Coalfield",
     "state": "Jharkhand",
+    "country": "India",
     "minetype": "coal",
     "lat": 23.7517,
     "lng": 86.4203,
-    "hasPdf":true
-
+    "hasPdf": true
   },
   {
     "name": "Raniganj Coalfield",
     "state": "West Bengal / Jharkhand",
+    "country": "India",
     "minetype": "coal",
     "lat": 23.75,
-    "lng": 87.0167
+    "lng": 87.0167,
+    "hasPdf": true
   },
   {
     "name": "Talcher Coalfield",
     "state": "Odisha",
+    "country": "India",
     "minetype": "coal",
     "lat": 20.95,
-    "lng": 85.22
+    "lng": 85.22,
+    "hasPdf": true
   },
   {
     "name": "Korba Coalfield",
     "state": "Chhattisgarh",
+    "country": "India",
     "minetype": "coal",
     "lat": 22.2,
-    "lng": 82.3
+    "lng": 82.3,
+    "hasPdf": true
   },
   {
     "name": "East Bokaro Coalfield",
     "state": "Jharkhand",
+    "country": "India",
     "minetype": "coal",
     "lat": 23.7689,
-    "lng": 85.9716
+    "lng": 85.9716,
+    "hasPdf": true
   },
   {
     "name": "Ib Valley Coalfield",
     "state": "Odisha",
+    "country": "India",
     "minetype": "coal",
     "lat": 21.68333,
-    "lng": 84.16667
+    "lng": 84.16667,
+    "hasPdf": true
   },
   {
     "name": "Singrauli Coalfield",
     "state": "Madhya Pradesh / Uttar Pradesh",
+    "country": "India",
     "minetype": "coal",
-    "lat": 24.7,
-    "lng": 81.8
+    "lat": 24.6,
+    "lng": 81.85,
+    "hasPdf": true
   },
   {
     "name": "Neyveli Lignite Mines",
     "state": "Tamil Nadu",
+    "country": "India",
     "minetype": "coal",
     "lat": 11.6,
-    "lng": 79.5
+    "lng": 79.5,
+    "hasPdf": true
   },
   {
     "name": "Kothagudem Coalfield",
     "state": "Telangana",
+    "country": "India",
     "minetype": "coal",
     "lat": 17.55,
-    "lng": 80.65
+    "lng": 80.65,
+    "hasPdf": true
   },
   {
     "name": "Hasdeo Arand Coalfield",
     "state": "Chhattisgarh",
+    "country": "India",
     "minetype": "coal",
     "lat": 22.8,
-    "lng": 82.45
+    "lng": 82.45,
+    "hasPdf": true
   },
   {
-    "name": "Jayant (OCP) Mine",
+    "name": "Jayant Opencast Project",
     "state": "Madhya Pradesh",
+    "country": "India",
     "minetype": "coal",
     "lat": 24.19468,
-    "lng": 82.68212
+    "lng": 82.68212,
+    "hasPdf": true
   },
   {
     "name": "Amrapali Opencast Mine",
     "state": "Jharkhand",
+    "country": "India",
     "minetype": "coal",
     "lat": 23.88948,
-    "lng": 85.00171
+    "lng": 85.00171,
+    "hasPdf": true
   },
   {
     "name": "Bhubaneshwari Opencast Mine",
     "state": "Odisha",
+    "country": "India",
     "minetype": "coal",
     "lat": 20.9617,
-    "lng": 85.1601
+    "lng": 85.1601,
+    "hasPdf": true
   },
   {
     "name": "Dudhichua Opencast Mine",
     "state": "Madhya Pradesh",
+    "country": "India",
     "minetype": "coal",
     "lat": 24.16474,
-    "lng": 82.67287
+    "lng": 82.67287,
+    "hasPdf": true
   },
   {
     "name": "Gevra Mine",
     "state": "Chhattisgarh",
+    "country": "India",
     "minetype": "coal",
     "lat": 22.35,
-    "lng": 82.65
+    "lng": 82.65,
+    "hasPdf": true
   },
   {
     "name": "Rampura Agucha Mine",
     "state": "Rajasthan",
+    "country": "India",
     "minetype": "zinc lead",
     "lat": 26.5,
-    "lng": 74.5
+    "lng": 74.5,
+    "hasPdf": true
   },
   {
-    "name": "Malanjkhand Copper Project",
-    "state": "Madhya Pradesh",
-    "minetype": "copper",
-    "lat": 22.02,
-    "lng": 80.71
-  },
-  {
-    "name": "Khetri Copper Mine (Khetri Nagar)",
+    "name": "Khetri Copper Mine ",
     "state": "Rajasthan",
+    "country": "India",
     "minetype": "copper",
     "lat": 27.98,
-    "lng": 75.8
+    "lng": 75.8,
+    "hasPdf": true
   },
   {
-    "name": "Bailadila Iron Ore Mines (Kirandul/Bacheli)",
+    "name": "Bailadila Iron Ore Mines ",
     "state": "Chhattisgarh",
+    "country": "India",
     "minetype": "iron_ore",
     "lat": 18.65,
-    "lng": 81.23
+    "lng": 81.23,
+    "hasPdf": true
   },
   {
-    "name": "Joda/Keonjhar Iron Ore Mines",
+    "name": "Joda Iron Ore Mines",
     "state": "Odisha",
+    "country": "India",
     "minetype": "iron_ore",
     "lat": 22.19,
-    "lng": 86.16
+    "lng": 86.16,
+    "hasPdf": true
   },
   {
     "name": "Singhbhum Copper-Iron Mines",
     "state": "Jharkhand",
+    "country": "India",
     "minetype": "copper_iron",
     "lat": 22.5,
-    "lng": 85.5
+    "lng": 85.5,
+    "hasPdf": true
   },
   {
     "name": "Kudremukh Iron Ore Mines",
     "state": "Karnataka",
+    "country": "India",
     "minetype": "iron_ore",
     "lat": 13.13,
-    "lng": 75.17
+    "lng": 75.17,
+    "hasPdf": true
   },
   {
     "name": "Balaghat Copper Mine",
     "state": "Madhya Pradesh",
+    "country": "India",
     "minetype": "copper",
     "lat": 22.02,
-    "lng": 80.71
+    "lng": 80.71,
+    "hasPdf": true
   },
   {
     "name": "Koraput Bauxite Mines",
     "state": "Odisha",
+    "country": "India",
     "minetype": "bauxite",
     "lat": 18.85,
-    "lng": 83.02
+    "lng": 83.02,
+    "hasPdf": true
   },
   {
     "name": "Balangir Bauxite Mines",
     "state": "Odisha",
+    "country": "India",
     "minetype": "bauxite",
     "lat": 20.88,
-    "lng": 82.85
+    "lng": 82.85,
+    "hasPdf": true
   },
   {
     "name": "Bargarh Bauxite Mines",
     "state": "Odisha",
+    "country": "India",
     "minetype": "bauxite",
     "lat": 21.2,
-    "lng": 83.37
+    "lng": 83.37,
+    "hasPdf": true
   },
   {
     "name": "Sundergarh Manganese Mines",
     "state": "Odisha",
+    "country": "India",
     "minetype": "manganese",
     "lat": 22.12,
-    "lng": 84.03
+    "lng": 84.03,
+    "hasPdf": true
   },
   {
-    "name": "Nagpur/Bhandara Manganese Mines",
+    "name": "Nagpur-Bhandara Manganese Mines",
     "state": "Maharashtra",
+    "country": "India",
     "minetype": "manganese",
     "lat": 21.54,
-    "lng": 79.68
+    "lng": 79.68,
+    "hasPdf": true
   },
   {
     "name": "Hazaribagh Copper Mine",
     "state": "Jharkhand",
+    "country": "India",
     "minetype": "copper",
     "lat": 23.98,
-    "lng": 85.35
+    "lng": 85.35,
+    "hasPdf": true
   },
-  
+  {
+    "name": "Bingham Canyon Mine",
+    "state": "Utah",
+    "country": "USA",
+    "minetype": "copper",
+    "lat": 40.523,
+    "lng": -112.151,
+    "hasPdf": true
+  },
+  {
+    "name": "Chuquicamata Mine",
+    "state": "Antofagasta Region",
+    "country": "Chile",
+    "minetype": "copper",
+    "lat": -22.292,
+    "lng": -68.903,
+    "hasPdf": true
+  },
+  {
+    "name": "Escondida Mine",
+    "state": "Antofagasta Region",
+    "country": "Chile",
+    "minetype": "copper",
+    "lat": -24.270,
+    "lng": -69.070,
+    "hasPdf": true
+  },
+  {
+    "name": "Grasberg Mine",
+    "state": "Papua",
+    "country": "Indonesia",
+    "minetype": "copper & gold",
+    "lat": -4.053,
+    "lng": 137.116,
+    "hasPdf": true
+  },
+  {
+    "name": "Mirny Diamond Mine",
+    "state": "Sakha Republic (Yakutia)",
+    "country": "Russia",
+    "minetype": "diamond",
+    "lat": 62.529,
+    "lng": 113.993,
+    "hasPdf": true
+  },
+  {
+    "name": "Kiruna Mine",
+    "state": "Norrbotten County",
+    "country": "Sweden",
+    "minetype": "iron",
+    "lat": 67.855,
+    "lng": 20.225,
+    "hasPdf": true
+  },
+  {
+    "name": "Garzweiler Mine",
+    "state": "North Rhine-Westphalia",
+    "country": "Germany",
+    "minetype": "lignite (coal)",
+    "lat": 51.043,
+    "lng": 6.502,
+    "hasPdf": true
+  },
+  {
+    "name": "Muruntau Mine",
+    "state": "Navoi Region",
+    "country": "Uzbekistan",
+    "minetype": "gold",
+    "lat": 40.558,
+    "lng": 64.601,
+    "hasPdf": true
+  },
+  {
+    "name": "Cerro Verde Mine",
+    "state": "Arequipa Region",
+    "country": "Peru",
+    "minetype": "copper",
+    "lat": -16.526,
+    "lng": -71.615,
+    "hasPdf": true
+  },
+  {
+    "name": "Fimiston Open Pit",
+    "state": "Western Australia",
+    "country": "Australia",
+    "minetype": "gold",
+    "lat": -30.775,
+    "lng": 121.509,
+    "hasPdf": true
+  }
 ]
+
 export default mines;
+
